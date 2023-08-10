40e4acf3f3f4b6fd173e35b8160a8fe6e00f4f2d
thuandinhce <congthuan.ce@gmail.com>
2023-08-10 05:38:13 -0400

    fix W293 blank line contains whitespace

--- service/routes.py
+++ service/routes.py
@@ -124,7 +124,7 @@ def delete_accounts(account_id):
     if account:
         account.delete()
     return "", status.HTTP_204_NO_CONTENT
-    
+
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
